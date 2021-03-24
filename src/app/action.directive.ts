75533685b7b14f6442cd1798be633dbbf3921854
greenyourcode <greenyourcode@gmail.com>
2021-03-24 21:18:32 +0100

    edit change

--- src/app/action.directive.ts
+++ src/app/action.directive.ts
@@ -1,14 +1,15 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appAction]'
 })
 export class ActionDirective {
-
+  @Input('appAction') color: string;
+  
   constructor(private el: ElementRef) { }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('red');
+    this.highlight(this.color);
   }
 
   @HostListener('mouseleave') onMouseLeave() {
