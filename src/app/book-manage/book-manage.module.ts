f229f46d04e18001b7698eeae218aef41f0ad72e
greenyourcode <greenyourcode@gmail.com>
2021-03-24 23:54:54 +0100

    add modules

--- src/app/book-manage/book-manage.module.ts
+++ src/app/book-manage/book-manage.module.ts
@@ -7,8 +7,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { ActionDirective } from '../action.directive';
 import { StkPipe } from '../stk.pipe';
 
-
-
 @NgModule({
   declarations: [
     BookEditComponent,
