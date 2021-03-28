ffb1e0e8e11a9e8004dd8ab866eb0e9f36bd5853
greenyourcode <greenyourcode@gmail.com>
2021-03-28 22:29:23 +0200

    add animations

--- src/app/order-manage/order-manage.component.ts
+++ src/app/order-manage/order-manage.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component } from '@angular/core';
 import { interval } from 'rxjs';
 import { takeWhile, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-order-manage',
   templateUrl: './order-manage.component.html',
-  styleUrls: ['./order-manage.component.scss']
+  styleUrls: ['./order-manage.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('2s')
+      ]),
+      transition('closed => open', [
+        animate('1s')
+      ]),
+    ])
+  ]
 })
 export class OrderManageComponent {
   interval = 0;
   subscription: any;
+  isOpen = true;
   state: string = 'inactive';
 
   constructor() { }
@@ -18,8 +40,9 @@ export class OrderManageComponent {
     this.subscription = interval(10).pipe(
       takeWhile(() => this.state !== 'active'),
       tap(() => {
-        if (this.interval === 4000) {
+        if (this.interval === 400) {
           this.state = 'active';
+          this.isOpen = false;
         }
       })
     ).subscribe((x) => this.interval = x);
--- src/app/order-manage/order-manage.module.ts
+++ src/app/order-manage/order-manage.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderManageComponent } from './order-manage.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
 @NgModule({
   declarations: [OrderManageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ]
 })
 export class OrderManageModule { }
