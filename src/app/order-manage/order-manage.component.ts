2dcf1316ac8f05b57ff9c95726b92ff18f3c082f
greenyourcode <greenyourcode@gmail.com>
2021-03-28 21:33:45 +0200

    add features

--- src/app/order-manage/order-manage.component.ts
+++ src/app/order-manage/order-manage.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { takeWhile, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-order-manage',
   templateUrl: './order-manage.component.html',
   styleUrls: ['./order-manage.component.scss']
 })
-export class OrderManageComponent implements OnInit {
+export class OrderManageComponent {
+  interval = 0;
+  subscription: any;
+  state: string = 'inactive';
 
   constructor() { }
 
-  ngOnInit(): void {
+  start() {
+    this.subscription = interval(10).pipe(
+      takeWhile(() => this.state !== 'active'),
+      tap(() => {
+        if (this.interval === 4000) {
+          this.state = 'active';
+        }
+      })
+    ).subscribe((x) => this.interval = x);
   }
-
 }
