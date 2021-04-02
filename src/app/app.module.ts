5ac86bc8ce469a125b45becfb44b01e5ca04e161
greenyourcode <greenyourcode@gmail.com>
2021-04-02 20:21:51 +0200

    update data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,8 @@ import { OrderManageModule } from './order-manage/order-manage.module';
 import { BookManageModule } from './book-manage/book-manage.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ActionDirective } from './action.directive';
-import { StkPipe } from './stk.pipe';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
